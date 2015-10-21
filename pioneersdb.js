f4984173b430ed177110795b0212dbffe56cbfbf
Aseem Keyal <akeyal@berkeley.edu>
2015-10-20 21:11:48 -0700

    crude admin register and approval system

--- pioneersdb.js
+++ pioneersdb.js
@@ -1,4 +1,5 @@
 if (Meteor.isClient) {
+    Meteor.subscribe("users");
     Template.adminRegister.events({
         'submit form': function(event) {
             event.preventDefault();
@@ -26,21 +27,36 @@ if (Meteor.isClient) {
         }
     });
     Template.dashboard.events({
-        'click .logout': function(event){
+        'click .logout': function(event) {
             event.preventDefault();
             Meteor.logout();
         }
     });
     Template.dashboard.helpers({
-          "user": function() {  return Meteor.users.find(); },
-          "userEmail": function() {
-              console.log(this.emails[0].address);
-              return this.emails[0].address;
-          }
+        "user": function() {  return Meteor.users.find(); }
+    });
+    Template.potentialAdmin.helpers({
+        "userEmail": function() {
+            return this.emails[0].address;
+        }
+    });
+    Template.potentialAdmin.events({
+        'click .approve': function (event) {
+            event.preventDefault();
+            Meteor.users.update({_id: this._id}, {$set: {'profile.approved': 1 }});
+        }
     });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-  });
+    Meteor.startup(function () {
+    });
+    Meteor.publish("users", function () {
+        return Meteor.users.find();
+    });
+    Meteor.users.allow({
+        'update': function (userId,doc) {
+            return true;
+        }
+    });
 }
